06e2f57414b28198cf898c6ba0b95238cc651e9d
MazeQhi404 <maze.putuma@gmail.com>
2025-11-20 18:05:17 +0200

    defined five pieces of state to control data and UI filters

--- portfolio-podcast-app/src/pages/Home.jsx
+++ portfolio-podcast-app/src/pages/Home.jsx
@@ -6,3 +6,16 @@ import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
 import PodcastCard from '../components/PodcastCard' 
 import RecommendedCarousel from '../components/RecommendedCarousel'
+
+// Main Home Page Component:
+export default function Home() {
+    // State for data and UI controls
+    const [shows, setShows] = useState([])           // All podcast shows from API
+    const [loading, setLoading] = useState(true)     // Show loading message while fetching
+    const [search, setSearch] = useState('')         // Search input text
+    const [sortBy, setSortBy] = useState('updated' ) // Sort option: updated, az, za
+    const [selectedGenre, setSelectedGenre] = useState('all') //Selected genre filter
+
+    
+
+}
