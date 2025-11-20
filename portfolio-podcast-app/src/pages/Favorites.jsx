cd0e3d3a77e8b25ef60b567439d28a9c7d89cdc5
MazeQhi404 <maze.putuma@gmail.com>
2025-11-20 21:57:22 +0200

    implemented Favourites page with grouped episodes, added date, and remove button

---
+++ portfolio-podcast-app/src/pages/Favorites.jsx
@@ -0,0 +1,23 @@
+import { Link } from 'react-router-dom'
+import useFavorites from '../hooks/useFavorites'
+import { FiTrash2 } from 'react-icons/fi'
+import { format } from 'date-fns'
+
+export default function Favorites() {
+    const { favorites, toggleFavorite } = useFavorites()
+
+    const grouped = favorites.reduce((acc, ep) => {
+        if (!acc[ep.showTitle]) acc[ep.showTitle] = []
+        acc[ep.showTitle].push(ep)
+        return acc
+    }, {})
+
+    if (favorites.length === 0) {
+        return (
+            <div className="text-center py-20">
+                <p className="text-xl">No Favourite Episodes Yet 🖤</p>
+                <Link to="/" className="text-purple-600 hover:underline">Browse Podcasts</Link>
+            </div>
+        )
+    }
+}
