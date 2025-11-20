bb6a34786573296730882320b621db3ad226c8be
MazeQhi404 <maze.putuma@gmail.com>
2025-11-20 17:27:02 +0200

    added sticky header with logo, favorites link, and theme toggle

---
+++ portfolio-podcast-app/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom'
+import { useTheme } from '../contexts/ThemeContext'
+import { FiSun, FiMoon, FiHeart, FiSearch } from 'react-icons/fi'
+
+export default function Header() {
+    //Gets current theme and toggle function from ThemeContext
+    const { theme, toggleTheme} = useTheme()
+
+    return (
+        
+    )
+}
