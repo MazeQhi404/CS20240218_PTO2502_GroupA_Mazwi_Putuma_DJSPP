83bfba9666675b43cf36007e917fee5a30c56113
MazeQhi404 <maze.putuma@gmail.com>
2025-11-21 23:22:31 +0200

    add useAudio hook that exposes AudioContext safely

--- portfolio-podcast-app/src/hooks/useAudio.js
+++ portfolio-podcast-app/src/hooks/useAudio.js
@@ -1,12 +1,8 @@
 import { useContext } from "react";
-import { AudioContext } from '../contexts/AudioContext'
+import { AudioContext } from '../contexts/AudioContext';
 
-// Custom Hook to safely access the AudioContext
 export default function useAudio() {
-    const context = useContext(AudioContext)
-
-    // Prevents bugs - gives clear error incase someone forgets to wrap with <AudioProvider>
-    if (!context) throw new error('useAudio must be used within AudioProvider')
-    
-    return context    
+  const context = useContext(AudioContext);
+  if (!context) throw new Error('useAudio must be used within AudioProvider');
+  return context;
 }
