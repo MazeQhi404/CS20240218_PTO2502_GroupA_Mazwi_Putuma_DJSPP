4f82b24551b681e642f56bf882b47c011824c3b3
MazeQhi404 <maze.putuma@gmail.com>
2025-11-21 23:28:19 +0200

    set up vite with installed dependencies as well as tailwind for styling

---
+++ portfolio-podcast-app/vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'  // v4 Vite plugin
+
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(),  //This processes Tailwind in v4
+  ],
+})
